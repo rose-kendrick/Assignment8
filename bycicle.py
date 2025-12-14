6788892d8b8addb8f4f164013d86b8d4aa89af9c
R K <rk@public-9-185.emich.edu>
2025-12-14 17:22:31 -0500

    added viscous drag force and added some hills to have the force of gravity as well

--- bycicle.py
+++ bycicle.py
@@ -18,9 +18,18 @@ def F_dragV(v_i):
     F = -n*A*(v_i/h)
     return F
 
-def dvdt(P,m,v_i):
-    a = ((P/(m*v_i))+((F_drag(v_i))/m)+((F_dragV(v_i))/m))
+def dvdt(P,m,v_i,x_i):
+    a = ((P/(m*v_i))+((F_drag(v_i))/m)+((F_dragV(v_i))/m)+((F_g(theta(x_i)))/m))
     return a
+
+def theta(x): #deg
+    angle = np.arctan(0.08*(0.15*np.sin(0.015*x+3)+0.6*np.sin(0.025*x+5)+0.3*np.sin(0.009*x)))
+    return angle
+
+def F_g(theta_i):
+    F = m*g*np.sin(theta_i)
+    return F
+
 
 v0 = 4 #m/s
 m = 70 #kg
@@ -33,20 +42,25 @@ A = 0.33 #m^2
 rho = 1.225 #kg/m^3 density of air at standard conditions
 n = 2e-5 #Pa*s
 h = 2 #m
+g = -9.81 #m/s
 
 
 N = int(tmax/dt)+1
 t = np.linspace(t0,tmax,N)
 v = np.zeros(N)
 v[0] = v0
+x = np.zeros(N)
 
 
 for i in range(N-1):
-    v[i+1] = v[i]+dvdt(P,m,v[i])*dt
+    x[i+1] = x[i]+v[i]*dt
+    v[i+1] = v[i]+dvdt(P,m,v[i],x[i])*dt
+
+
 
 pp.plot(t,v,)
 pp.title('Velocity, v, as a function of time, t')
 pp.xlabel(r'time, $t (s)$')
 pp.ylabel(r'velocity, $v (\frac{m}{s})$')
 # pp.show()
-pp.savefig('bikeWithDrag2.png')
+pp.savefig('bikeWithDrag3.png')
