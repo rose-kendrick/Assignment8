0e1f81c716e3da0cfd8673c3bcbd84d3208256dc
R K <rk@Rs-MacBook-Pro.local>
2025-12-14 20:26:07 -0500

    Modified to find the mean squared for each walker. Plotted this value for each step number

--- random1.py
+++ random1.py
@@ -5,27 +5,40 @@ import random
 
 nsteps = int(input('Give a number of steps: '))
 nwalkers = 500
-x_finals = []
+
+
+x_current = np.zeros((nwalkers,nsteps+1))
+steps = np.arange(nsteps+1)
 
 for walker in range(nwalkers):
-    
     x = [0]
-    steps = [0]
-    for step in range(nsteps):
-        x.append(x[-1]+random.randint(-1,1))
-        steps.append(step+1)
+    x_current[walker,0] = x[0]
 
-    x_finals.append(x[-1])
+    for step in range(1,nsteps+1):
+        x.append(x[-1] + random.randint(-1,1))
+        x_current[walker,step] = x[-1]
+
+    # x_finals.append(x[-1])
     
     pp.plot(steps,x)
+
+
+mean_displacement = x_current.mean(axis=0)
+mean2 = np.square(mean_displacement)
+
 
 pp.xlabel('Step Number')
 pp.ylabel('Position (m)')
 pp.savefig('randomwalk500.png')
 # pp.show()
 
-x_finals_sum = sum(x_finals)
-x_n = x_finals_sum/nwalkers
-x_n2 = x_n**2
-print(x_n,x_n2)
+
+pp.figure()
+pp.plot(steps, mean2, label='mean')
+pp.xlabel('Step Number')
+pp.ylabel('Mean position (m)')
+# pp.show()
+pp.savefig('displacement.png')
+
+
 
