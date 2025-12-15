ee0dd2fdf34e0fe8676d7d8a95551435b8571194
R K <rk@Rs-MacBook-Pro.local>
2025-12-14 19:08:27 -0500

    modiefied to solve for 500 walkers and calculated mean displacement, x_n, and mean over all walkers, x_n^2.

--- random1.py
+++ random1.py
@@ -4,7 +4,8 @@ import random
 
 
 nsteps = int(input('Give a number of steps: '))
-nwalkers = 1
+nwalkers = 500
+x_finals = []
 
 for walker in range(nwalkers):
     
@@ -13,9 +14,18 @@ for walker in range(nwalkers):
     for step in range(nsteps):
         x.append(x[-1]+random.randint(-1,1))
         steps.append(step+1)
+
+    x_finals.append(x[-1])
     
     pp.plot(steps,x)
-    pp.xlabel('Step Number')
-    pp.ylabel('Position (m)')
-    pp.savefig('randomwalk2.png')
-    # pp.show()
+
+pp.xlabel('Step Number')
+pp.ylabel('Position (m)')
+pp.savefig('randomwalk500.png')
+# pp.show()
+
+x_finals_sum = sum(x_finals)
+x_n = x_finals_sum/nwalkers
+x_n2 = x_n**2
+print(x_n,x_n2)
+
